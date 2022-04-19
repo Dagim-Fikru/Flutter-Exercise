50cec6962403b3ccfa0b58d2e7fc395222f682f4
Dagim Fikru <dagimfikru@gmail.com>
2022-04-19 00:20:55 +0000

    done with appbar

--- id/lib/main.dart
+++ id/lib/main.dart
@@ -1,4 +1,21 @@
+// ignore_for_file: camel_case_types, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-void main() => runApp()
+void main() => runApp(MaterialApp(
+  home: simpleId(),
+));
+class simpleId extends StatelessWidget {
+  const simpleId({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Simple Id'),
+        backgroundColor: Colors.brown,
+        centerTitle: true,
+      ),
+    );
+  }
+}
