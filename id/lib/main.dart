21c81abc4103538d61b96df92a2ba15c044e10f7
Dagim Fikru <dagimfikru@gmail.com>
2022-04-19 01:13:47 +0000

    ..

--- id/lib/main.dart
+++ id/lib/main.dart
@@ -12,11 +12,40 @@ class simpleId extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 3, 58, 53),
       appBar: AppBar(
         title: Text('Simple Id'),
         backgroundColor: Colors.brown,
         centerTitle: true,
         elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            Text(
+              'Name:',
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+              width: 10.0,
+            ),
+            Text(
+              'Dagim Fikru',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 175, 154, 68),
+                  letterSpacing: 2.0,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
       ),
     );
   }
